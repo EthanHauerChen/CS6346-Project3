269c450356f949dd58693664cac1cb85eaeb519c
EthanHauerChen <ethanhchen01@gmail.com>
2025-08-06 18:36:01 -0500

    fixed errors

--- src/main.cu
+++ src/main.cu
@@ -4,7 +4,7 @@
 
 int main() {
     FindSamples sampling_object{};
-    sampling_object.create_samples();
+    sampling_object.create_samples(1, 2, 3, 4, 100);
 
     return 0;
 }
--- src/sampling.h
+++ src/sampling.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdio.h>
 #include <iostream>
 #include <cmath>
@@ -8,7 +10,7 @@
 
 namespace Kernels {
     __global__ void calculateY(float* y_values, float w0, float w1, float w2, float b, uint16_t numSamples, float stride) {
-        unsigned long long global_index = blockIdx * 32 + threadIdx;
+        unsigned long long global_index = blockIdx.x * 32 + threadIdx.x;
         if (global_index > numSamples) return;
 
         float xValue = stride * global_index - 10;
@@ -23,7 +25,7 @@ struct FindSamples {
         dim3 threads_per_block(32, 1, 1);
         int block_count = std::ceil((float)numSamples / (float)threads_per_block.x);
         dim3 blocks_per_grid(block_count, 1, 1);
-        float stride = (20f / (float)numSamples);
+        float stride = (20.0 / (float)numSamples);
         size_t numbytes_in_array = numSamples * sizeof(float);
 
         float* cpu_samples = (float*)malloc(numbytes_in_array);
@@ -31,7 +33,7 @@ struct FindSamples {
         float* gpu_samples;
         cudaMalloc(&gpu_samples, numbytes_in_array);
         
-        Kernels::calculateY<<<blocks_per_grid, threads_per_block>>>(gpu_samples, w0, w1, w2, b, stride);
+        Kernels::calculateY<<<blocks_per_grid, threads_per_block>>>(gpu_samples, w0, w1, w2, b, numSamples, stride);
         cudaDeviceSynchronize();
         cudaMemcpy(cpu_samples, gpu_samples, numbytes_in_array);
         cudaFree(gpu_samples);
@@ -46,8 +48,8 @@ struct FindSamples {
             std::cout << "type an x value to check the polynomial\n";
             std::cin >> num;
             std::cout << "here is the corresponding y for the x = {" << num * numSamples / 20 << "}: ";
-            num = (num + 10) * numSamples / 20;
+            num = (int)((num + 10) * numSamples / 20);
             std::cout << cpu_samples[num] << "\n";
         }
     }
-}
+};
