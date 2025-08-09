a3d8bbf4d23e3b4cf9a3ca30ab107da8e917d947
Ethan <ethanhchen01@gmail.com>
2025-08-09 05:15:14 -0500

    more tests

--- src/inflection.h
+++ src/inflection.h
@@ -73,9 +73,6 @@ struct FindInflections {
         
         Kernels::calc_second_derivative<<<blocks_per_grid, threads_per_block>>>(gpu_derivatives, w0, w1, numSamples, stride, job_size);
         cudaMemcpy(cpu_derivatives, gpu_derivatives, numbytes_in_polynomial, cudaMemcpyDeviceToHost);
-        //testing stuff
-        cpu_derivatives[516666665] = -1;
-        cpu_derivatives[516666667] = 1;
         cudaMemcpy(gpu_derivatives, cpu_derivatives, numbytes_in_polynomial, cudaMemcpyHostToDevice);
         Kernels::search_inflection_points<<<blocks_per_grid, threads_per_block>>>(gpu_derivatives, gpu_inflection, numSamples, stride, job_size);
         cudaMemcpy(cpu_inflection, gpu_inflection, numbytes_in_bool, cudaMemcpyDeviceToHost);
@@ -94,9 +91,11 @@ struct FindInflections {
             printf("%s\n", cpu_inflection[i] ? "true" : "false");
         }
         std::cout << "the polynomial at x = {.8333333} has inflection point: "; //hard coded scaling 100 samples to values from [-10, 10]
-        printf("%s\n", cpu_inflection[458333333] ? "true" : "false");
+        printf("%f\n", cpu_inflection[458333332]);
         std::cout << "the polynomial at x = {.8333333} has inflection point: "; //hard coded scaling 100 samples to values from [-10, 10]
-        printf("%s\n", cpu_inflection[458333332] ? "true" : "false");
+        printf("%f\n", cpu_inflection[458333333]);
+        std::cout << "the polynomial at x = {.8333333} has inflection point: "; //hard coded scaling 100 samples to values from [-10, 10]
+        printf("%f\n", cpu_inflection[458333334]);
         return cpu_inflection;
     }
 };
