d84e8657126c68ccba77898b10157356ef16098f
Ethan <ethanhchen01@gmail.com>
2025-08-11 01:06:59 -0500

    render graph, test with 100K samples

--- src/main.cu
+++ src/main.cu
@@ -49,8 +49,8 @@ void render_graph(double* samples, uint32_t numSamples) {
 }
 
 int main(int argc, char* argv[]) {
-    polynomial p{.2, .5, -1, 0};
-    uint32_t numSamples = 1000;
+    polynomial p{.15, -1, .5, 1};
+    uint32_t numSamples = 100000;
     uint16_t num_jobs = 1;
     FindSamples sampling_object{};
     FindInflections inflection_object{};
